e4088d9d4aeebb0f21c40e18ec18734c870cf410
Nate Webber <nwebber@u.rochester.edu>
2023-02-02 18:48:56 -0500

    implement skeleton UI

--- ui.py
+++ ui.py
@@ -1,15 +1,47 @@
 import dearpygui.dearpygui as dpg
 
-dpg.create_context()
-dpg.create_viewport(title="hopped up out the bed", width=600, height = 300)
+"""
+Gather user-input parameters and pass them to the "backend" to run the rendering process
+"""
 
-with dpg.window(label="turn my swag on"):
-    dpg.add_text("took a look in the mirror")
-    dpg.add_button(label="said what's up")
-    dpg.add_input_text(label="string", default_value="yeah i'm gettin' money")
-    dpg.add_slider_float(label="oh", default_value=0.273, max_value=1)
-    
+
+def render_video():
+    print("someday I will actually render the video")
+
+
+dpg.create_context()
+dpg.create_viewport(title='EasyScore', width=600, height=600)
+
+with dpg.window(tag="Main"):
+    with dpg.collapsing_header(label="Image", default_open=True):
+        with dpg.tab_bar():
+            with dpg.tab(label="Internet"):
+                dpg.add_input_text(label="Image URL",
+                                   tag="input_image_remote_url")
+            with dpg.tab(label="Local"):
+                dpg.add_text("Select an image file")
+
+    with dpg.collapsing_header(label="Audio", default_open=True):
+        with dpg.tab_bar():
+            with dpg.tab(label="YouTube"):
+                dpg.add_input_text(label="Youtube URL",
+                                   tag="input_audio_remote_url")
+            with dpg.tab(label="Local"):
+                dpg.add_text("Select an audio file")
+
+    with dpg.collapsing_header(label="Options/Extras", default_open=False):
+        dpg.add_text("Extra stuff here")
+        # "options/extras" will include:
+        # video length
+        # choosing where audio starts from
+        # choosing to delete downloads after process is complete
+        # other things that i'm forgetting at the moment
+
+    with dpg.collapsing_header(label="Run", default_open=True):
+        dpg.add_button(label="Run", callback=render_video)
+
 dpg.setup_dearpygui()
 dpg.show_viewport()
+dpg.set_primary_window("Main", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
