9e64e4be9ff112d79aaee5a6a778b5d8dadc77a4
Nate Webber <nwebber@u.rochester.edu>
2023-02-01 22:43:53 -0500

    take user input for image/audio files

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from moviepy.editor import * 
 
-clip = ImageClip("images/meme.jpg")
-audio_clip = AudioFileClip("audio/metal.mp3")
+image_path = input("Input the filename of your image (must be in 'images' directory): ")
+audio_path = input("Input the filename of your audio (must be in 'audio' directory): ")
+video_length = input("Input video length (in seconds): ")
+output_name = input("Input filename for output: ")
+
+clip = ImageClip(f"images/{image_path}")
+audio_clip = AudioFileClip(f"audio/{audio_path}")
 
 clip.audio = audio_clip
 
-clip = clip.set_duration(10)
+clip = clip.set_duration(video_length).resize(width=800)
 
-clip.write_videofile("test.mp4", 1) 
+clip.write_videofile(f"{output_name}.mp4", 1)
