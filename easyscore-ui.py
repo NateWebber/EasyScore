389c4492e724ec64f9e0e5eeace0146275646aa2
Nate Webber <nwebber@u.rochester.edu>
2023-02-03 18:07:17 -0500

    aimplement output filename selection, rearrange ui elements

--- easyscore-ui.py
+++ easyscore-ui.py
@@ -4,13 +4,20 @@ import dearpygui.dearpygui as dpg
 from renderer import Renderer
 from es_enums import AudioSource, ImageSource
 
+
 def render_video():
+    dpg.set_value("ui_status_text", "Rendering...")
+    dpg.show_item("ui_status_text")
     selected_image_path = None
     selected_image_source = None
     selected_audio_path = None
     selected_audio_source = None
 
-    #set image source/path variables
+    output_file_name = dpg.get_value("input_output_file_name")
+    if (output_file_name == "" or output_file_name == None):
+        output_file_name = "out"
+
+    # set image source/path variables
     match dpg.get_value("ui_image_tabs"):
         case "ui_image_tab_internet":
             selected_image_path = dpg.get_value("input_image_remote_url")
@@ -20,7 +27,7 @@ def render_video():
                 "input_image_local_file")['file_path_name']
             selected_image_source = ImageSource.LOCAL
 
-    #set audio source/path variables
+    # set audio source/path variables
     match dpg.get_value("ui_audio_tabs"):
         case "ui_audio_tab_youtube":
             selected_audio_path = dpg.get_value("input_audio_remote_url")
@@ -30,16 +37,18 @@ def render_video():
                 "input_audio_local_file")['file_path_name']
             selected_audio_source = AudioSource.LOCAL
 
-    #instantiate renderer and render video
+    # instantiate renderer and render video
     renderer = Renderer(selected_image_source, selected_image_path,
-                        selected_audio_source, selected_audio_path, dpg.get_value("input_video_duration"))
+                        selected_audio_source, selected_audio_path, dpg.get_value("input_video_duration"), output_file_name)
     renderer.render()
     print("Rendering complete!")
+    dpg.set_value("ui_status_text", "Rendering complete!")
 
-    #delete downloaded files if selected
+    # delete downloaded files if selected
     if (dpg.get_value("ui_delete_downloads")):
         if (os.path.exists("dl")):
             shutil.rmtree("dl")
+
 
 def run():
     dpg.create_context()
@@ -53,11 +62,11 @@ def run():
                 # Internet Tab
                 with dpg.tab(label="Internet", tag="ui_image_tab_internet"):
                     dpg.add_input_text(label="Image URL",
-                                    tag="input_image_remote_url")
+                                       tag="input_image_remote_url")
                 # Local Tab
                 with dpg.tab(label="Local", tag="ui_image_tab_local"):
                     dpg.add_button(label="Select an image file", tag="ui_open_image_file_dialog",
-                                callback=lambda: dpg.show_item("input_image_local_file"))
+                                   callback=lambda: dpg.show_item("input_image_local_file"))
                     with dpg.file_dialog(directory_selector=False, tag="input_image_local_file", show=False, width=500, height=300):
                         dpg.add_file_extension(
                             "Image files (*.png *.jpg *.bmp){.png,.jpg,.bmp}")
@@ -68,37 +77,43 @@ def run():
                 # Youtube Tab
                 with dpg.tab(label="YouTube", tag="ui_audio_tab_youtube"):
                     dpg.add_input_text(label="Youtube URL",
-                                    tag="input_audio_remote_url")
+                                       tag="input_audio_remote_url")
                 # Local Tab
                 with dpg.tab(label="Local", tag="ui_audio_tab_local"):
                     dpg.add_button(label="Select an audio file", tag="ui_open_audio_file_dialog",
-                                callback=lambda: dpg.show_item("input_audio_local_file"))
+                                   callback=lambda: dpg.show_item("input_audio_local_file"))
                     with dpg.file_dialog(directory_selector=False, tag="input_audio_local_file", show=False, width=500, height=300):
                         dpg.add_file_extension(
                             "Audio files (*.mp3 *.wav *.ogg){.mp3,.wav,.ogg}")
 
         # Options/Extras Section
         with dpg.collapsing_header(label="Options/Extras", default_open=True):
-            # video length (may need to reconsider max value in future)
-            dpg.add_input_int(label="Video Duration (seconds)",
-                            default_value=15, min_value=0, max_value=9999, tag="input_video_duration")
-            # choosing where audio starts from
-            # choosing to delete downloads after process is complete
+            # choose video length (may need to reconsider max value in future)
+            dpg.add_text("Video Duration (seconds):")
+            dpg.add_input_int(default_value=15, min_value=0,
+                              max_value=9999, tag="input_video_duration")
+            # TODO choosing where audio starts from
+            # choose output name
+            dpg.add_text("Output filename:")
+            dpg.add_input_text(
+                label=".mp4", tag="input_output_file_name", default_value="out")
+            # choose to delete downloads after process is complete
             dpg.add_checkbox(label="Remove downloaded files after rendering? (Recommended)",
-                            tag="ui_delete_downloads", default_value=True)
+                             tag="ui_delete_downloads", default_value=True)
             # other things that i'm forgetting at the moment
 
         # Run Section
         with dpg.collapsing_header(label="Run", default_open=True):
             dpg.add_button(label="Run", callback=render_video,
-                        width=250, height=50)
-            #TODO figure this out
-            dpg.add_text("Eventually status text goes here", tag="ui_status_text")
+                           width=250, height=50)
+            # TODO figure this out
+            dpg.add_text("Rendering...", tag="ui_status_text", show=False)
 
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.set_primary_window("Main", True)
     dpg.start_dearpygui()
     dpg.destroy_context()
+
 
 run()
